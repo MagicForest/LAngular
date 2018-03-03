3b273928e35200c8bf2de74f5a37ab85bca146ad
Grass.Winter <quietwolf@yeah.net>
2018-03-03 08:01:13 +0800

    6.router-select a dashboard hero

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';// <-- NgModel lives here
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
@@ -11,12 +13,33 @@ import { HeroService }         from './hero.service';
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding wih [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding wih [(ngModel)]
+    RouterModule.forRoot([
+        {
+            path: 'heroes',
+            component: HeroesComponent
+        },
+        {
+            path: 'dashboard',
+            component: DashboardComponent
+        },
+        {
+            path: '',
+            redirectTo: '/dashboard',
+            pathMatch: 'full'
+        },
+        {
+            path: 'detail/:id',
+            component: HeroDetailComponent
+        }
+
+    ])
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
